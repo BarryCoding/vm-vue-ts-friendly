b8319db617549f110adefed25929faa39c6fc9c1
Springer <zxllks@yahoo.com>
2023-07-27 17:14:02 +0800

    L2 type inference

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, reactive, ref } from 'vue'
+import fetchCount from './fetchCount'
+
+interface AppInfo {
+  name: string
+  slogan: string
+}
+
+const count = ref<number | null>(null)
+
+const appInfo: AppInfo = reactive({
+  name: 'TS Counter',
+  slogan: 'Learning TS',
+})
+
+onMounted(() => {
+  fetchCount((initCount) => {
+    count.value = initCount
+  })
+})
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>{{ appInfo.name }}</h1>
+  <h2>{{ appInfo.slogan }}</h2>
+  <p>{{ count }}</p>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
